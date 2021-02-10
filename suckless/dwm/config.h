c80876af87700085da885b8c58b4fd1a8788d3ef
masrazi <razi.simas@gmail.com>
2021-02-10 14:03:10 +0700

    Remove volume dan brightness key

--- suckless/dwm/config.h
+++ suckless/dwm/config.h
@@ -1,10 +1,11 @@
 /* See LICENSE file for copyright and license details. */
-#include <X11/XF86keysym.h>
+/*#include <X11/XF86keysym.h>
 static const char *upvol[]   = { "/usr/bin/pactl", "set-sink-volume", "0", "+5%",     NULL };
 static const char *downvol[] = { "/usr/bin/pactl", "set-sink-volume", "0", "-5%",     NULL };
 static const char *mutevol[] = { "/usr/bin/pactl", "set-sink-mute",   "0", "toggle",  NULL };
 static const char *brupcmd[] = { "xbacklight", "-inc", "10", NULL };
 static const char *brdowncmd[] = { "xbacklight", "-dec", "10", NULL };
+*/
 
 /* appearance */
 static const unsigned int borderpx  = 1;        /* border pixel of windows */
@@ -73,11 +74,11 @@ static const char *prtscrcmd[] = { "flameshot", "gui", NULL};
 
 static Key keys[] = {
 	/* modifier                     key        function        argument */
-	{ 0, 						XF86XK_MonBrightnessUp, spawn, {.v = brupcmd} },
+	/*{ 0, 						XF86XK_MonBrightnessUp, spawn, {.v = brupcmd} },
 	{ 0, 						XF86XK_MonBrightnessDown, spawn, {.v = brdowncmd} },
 	{ 0,                       XF86XK_AudioLowerVolume, spawn, {.v = downvol } },
 	{ 0,                       XF86XK_AudioMute, spawn,        {.v = mutevol } },
-	{ 0,                       XF86XK_AudioRaiseVolume, spawn, {.v = upvol   } },
+	{ 0,                       XF86XK_AudioRaiseVolume, spawn, {.v = upvol   } },*/
 	{ 0, 							XK_Print, spawn, 		   {.v = prtscrcmd } },
 	{ MODKEY,                       XK_p,      spawn,          {.v = dmenucmd } },
 	{ MODKEY|ShiftMask,             XK_Return, spawn,          {.v = termcmd } },
